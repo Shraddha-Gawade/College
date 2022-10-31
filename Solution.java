978ba88403dda4c3a293816685742e1930d8256e
Shraddha-Gawade <112467225+Shraddha-Gawade@users.noreply.github.com>
2022-10-31 11:57:18 +0530

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,29 +1,19 @@
-class Solution {
-    public List<String> fizzBuzz(int n) {
+import java.util.*;
+public class Solution {  
+ public static void main(String[] args) {
+        String str1 = "The length of last word";
+		System.out.println("Original String: "+str1);
+        System.out.println("Length of the last word of the above string: "+length_Of_last_word(str1));
+    }
 
-        
-        List<String> ans = new ArrayList<String>();
-
-        for (int num = 1; num <= n; num++) {
-
-            boolean divisibleBy3 = (num % 3 == 0);
-            boolean divisibleBy5 = (num % 5 == 0);
-
-            if (divisibleBy3 && divisibleBy5) {
-               
-                ans.add("FizzBuzz");
-            } else if (divisibleBy3) {
-                
-                ans.add("Fizz");
-            } else if (divisibleBy5) {
-       
-                ans.add("Buzz");
-            } else {
-           
-                ans.add(Integer.toString(num));
-            }
+    public static int length_Of_last_word(String str1) {
+        int length_word = 0;
+        String[] words = str1.split(" ");
+        if(words.length>0) {
+            length_word = words[words.length-1].length();			
+        } else {
+            length_word = 0;
         }
-
-        return ans;
+        return length_word;
     }
 }
