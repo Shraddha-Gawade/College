5509c04cbc7e1573bd307c2a3c6ca719325f9f84
Shraddha-Gawade <112467225+Shraddha-Gawade@users.noreply.github.com>
2022-11-01 08:25:36 +0530

    Add files via upload

--- num.java
+++ num.java
@@ -1,15 +1,12 @@
 import java.util.*;
-class num
-{
- public static void main(String[] args)
-{
+class num{
+ public static void main(String[] args){
  Scanner a=new Scanner(System.in);
  int low=a.nextInt();
  int up=a.nextInt();
  List<Integer>li=new ArrayList<>();
  if(up>low){
- for(int i=low;i<=up;i++)
-{
+ for(int i=low;i<=up;i++){
  double sqrt=Math.sqrt(i);
 
  if(sqrt-Math.floor(sqrt)==0&&fun(i)<10)
@@ -19,8 +16,7 @@ class num
 
  System.out.println(li);
  }
-public static int fun(int i)
-{
+public static int fun(int i){
  int sum=0;
  while(i!=0){
  int rem=i%10;
